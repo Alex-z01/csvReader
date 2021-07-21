bcb55b297ef1cf939329909effab05d6dca32a1e
Alex Zaalishvili <alexzchicken@gmail.com>
2021-07-20 22:57:44 -0400

    Rewritten to support parsing script, improved search functions

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
+import os
+import parsing
 import pandas as pd
 
 fieldnames = ['Username', 'Password', 'Account Status', 'Region', 'Email', 'Summoner Name', 'BE', 'RP', 'RP Refunds',
               'Level', 'Curr Rank', 'Prev Rank', 'Champs', 'Skins', 'Last Played']
 
-# pathcsv = "bruh.csv"
+pathCSV = "bruh.csv"
 pd.set_option('display.max_rows', 0)
 pd.set_option('display.max_columns', 0)
 pd.set_option('display.width', 0)
 
 # Load csv file into variable df
-def load():
+def load(path):
     global df
-    df = pd.read_csv("bruh.csv")
+    df = pd.read_csv(pathCSV)
 
 
 def up(range1, range2):
     print(df[['Username', 'Password']][range1:range2])
 
 
-def numericalSearch(header, queryValue, numeration, order):
+def numericalSearch(header, opr, queryValue, order):
     result = 0
     queryValue = int(queryValue)
     if order == 'A':
@@ -29,44 +31,56 @@ def numericalSearch(header, queryValue, numeration, order):
     sortedCSV = df.sort_values(header, ascending=order)
     result = sortedCSV
 
-    if numeration == "more":
-        result = sortedCSV.loc[sortedCSV[header] > queryValue]
-    if numeration == "less":
-        result = sortedCSV.loc[sortedCSV[header] < queryValue]
+    result = sortedCSV.loc[parsing.operator_dict[opr](sortedCSV[header], queryValue)]
+
+    result.to_csv('result.csv', sep=',', encoding='utf-8')
+    openFile('result.csv')
     return result
 
 def regularSearch(header, order):
     if order == 'A':
         order = True
-    else:
+    if order == 'D':
         order = False
     result = df.sort_values(header, ascending=order)
     print(result)
 
-def randSearch(query):
-    # USING FOR LOOP SO IT SEARCHES THROUGH EVERY FIELD, IF IT COULD SEARCH THROUGH ENTIRE DF THATD BE BETTER
-    for field in fieldnames:
+def randSearch(query, field):
+    if(field != None):
+        field = field.replace('_', ' ')
+    if(fieldnames.__contains__(field)):
         new_df = df[df[field].str.contains(query, na=False)]
         new_df = new_df.sort_values('Username', ascending=True)
-        if(new_df.shape[0] != 0):
-            print(new_df)
+        if (new_df.shape[0] != 0):
+            new_df.to_csv('result.csv', sep=',', encoding='utf-8')
+            openFile('result.csv')
+        else:
+            print("No results")
+        return
+    else:
+        print("Unknown field")
+
+
+
+def openFile(fileName):
+    print('Opening', fileName + '...')
+    os.system(fileName)
 
 #    new_df = df[df['Username'].str.contains('z', na=False)] SEARCHES FOR CONTAINING STRING IN DF
 #    new_df = new_df.sort_values('Username', ascending=True) SORTS NEW DF ON USERNAME
 #    print(new_df) PRINTS IT
 
 
-
 # up()
 # search("RP", 200, "more")
-load()
-randSearch("Unverif")
+load(pathCSV)
+#randSearch("Unverif")
 #header = input("Enter header\n")
 #queryValue = input("Enter query\n")
 #numeration = input("More/Less\n")
 #order = input("Ascending/Descending (A/D)\n")
 #if(header == 'BE' or header == 'RP' or 'Level' or 'Champs' or 'Skins'):
-#    print(numericalSearch(header, queryValue, numeration, order))
+#    openFile(numericalSearch(header, queryValue, numeration, order))
 #else:
 #    regularSearch(header, order)
 
